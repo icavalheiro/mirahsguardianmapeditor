e1a44cbc06817405faca07e582c3c4a3a158e362
Ivan Schuaste Cavalheiro <ivanscavalheiro@hotmail.com>
2015-04-18 20:31:34 -0300

    Plugin files updated
    reorganized plugin files structure
    updated map class to support searching for pathable tiles only

--- Source code/Mirah's Guardian Map Editor/Assets/MapEditor/Plugin/Scripts/Map.cs
+++ Source code/Mirah's Guardian Map Editor/Assets/MapEditor/Plugin/Scripts/Map.cs
@@ -88,6 +88,18 @@ public class Map : MonoBehaviour
 		return _tilesInScene.Find(x => x.transform.position == p_position);
 	}
 
+	public List<TileObject> GetIaWalkableTiles()
+	{
+		List<TileObject> __toReturn = new List<TileObject>();
+		_tilesInScene.ForEach(x =>
+		{
+			if(x.tile.pathable)
+				__toReturn.Add(x);
+		});
+
+		return __toReturn;
+	}
+
 	public List<TileObject> GetTileObjectsByImageName(string p_imageName)
 	{
 		List<TileObject> __toReturn = new List<TileObject>();
